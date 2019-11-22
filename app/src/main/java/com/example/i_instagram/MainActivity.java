d54448c6a406ee5384a7f49008f43b5a9cb1faef
*ivangonzalezny <ivan.gonzalez.ny@gmail.com>
2019-11-21 22:19:03 -0500

    Posts Recycler View

--- app/src/main/java/com/example/i_instagram/MainActivity.java
+++ app/src/main/java/com/example/i_instagram/MainActivity.java
@@ -1,52 +1,24 @@
 package com.example.i_instagram;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
-import android.provider.MediaStore;
-import android.util.Log;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.Toast;
 
-import com.google.android.material.bottomnavigation.BottomNavigationItemView;
+import com.example.i_instagram.fragments.ComposeFragment;
+import com.example.i_instagram.fragments.PostsFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.parse.FindCallback;
-import com.parse.ParseException;
-import com.parse.ParseFile;
-import com.parse.ParseQuery;
-import com.parse.ParseUser;
-import com.parse.SaveCallback;
-
-import java.io.File;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
-
     BottomNavigationView bottomNavigationView;
 
     final FragmentManager fragmentManager = getSupportFragmentManager();
-
-    // define your fragments here
-//    final Fragment fragment1 = new FirstFragment();
-//    final Fragment fragment2 = new SecondFragment();
-//    final Fragment fragment3 = new ThirdFragment();
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,24 +30,33 @@ public class MainActivity extends AppCompatActivity {
         bottomNavigationView.setOnNavigationItemReselectedListener(new BottomNavigationView.OnNavigationItemReselectedListener() {
             @Override
             public void onNavigationItemReselected(@NonNull MenuItem menuItem) {
+                Fragment fragment;
                 switch (menuItem.getItemId()) {
                     case R.id.action_home:
                         // do something here
+                        fragment = new PostsFragment();
                         Toast.makeText(MainActivity.this, "Home", Toast.LENGTH_SHORT).show();
                         break;
                     case R.id.action_compose:
                         // do something here
                         Toast.makeText(MainActivity.this, "Compose", Toast.LENGTH_SHORT).show();
+                        fragment = new ComposeFragment();
                         break;
                     case R.id.action_profile:
                         // do something here
                         Toast.makeText(MainActivity.this, "Profile", Toast.LENGTH_SHORT).show();
+                        fragment = new ComposeFragment();
                         break;
-                    default: break;
+                    default:
+                        Toast.makeText(MainActivity.this, "Default", Toast.LENGTH_SHORT).show();
+                        fragment = new ComposeFragment();
+                        break;
                 }
+                fragmentManager.beginTransaction().
+                        replace(R.id.flContainer, fragment).commit();
             }
         });
-
+        bottomNavigationView.setSelectedItemId(R.id.action_home);
     }
 }
 
